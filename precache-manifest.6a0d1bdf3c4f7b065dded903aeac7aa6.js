b9f6d9d3d451d2d7316f369f32ce62c0cbee35ae
Abhishek Kovvuri <abhishek.kovvuri@gmail.com>
2020-02-10 14:54:05 -0500

    Updates

---
+++ precache-manifest.6a0d1bdf3c4f7b065dded903aeac7aa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53a26889c5433b58110d92392f6b249",
+    "url": "/react-get-social/index.html"
+  },
+  {
+    "revision": "d29d788e548a07110717",
+    "url": "/react-get-social/static/css/main.04d3f15f.chunk.css"
+  },
+  {
+    "revision": "ef9c7b1ae617b1dae3cc",
+    "url": "/react-get-social/static/js/2.04308a13.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-get-social/static/js/2.04308a13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29d788e548a07110717",
+    "url": "/react-get-social/static/js/main.78109a87.chunk.js"
+  },
+  {
+    "revision": "56feccc863acdd49d354",
+    "url": "/react-get-social/static/js/runtime-main.f79c8766.js"
+  }
+]);
